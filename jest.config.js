b5c203dae5bf76e94f0fe53d691c6724b02003a2
Igor Atilar <igoratilar10@gmail.com>
2022-07-10 22:34:21 -0300

    feat: initial setup

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts']
+};
---
+++ src/main/index.tsx
@@ -0,0 +1,3 @@
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<div>Hello there!</div>, document.getElementById('root'));
