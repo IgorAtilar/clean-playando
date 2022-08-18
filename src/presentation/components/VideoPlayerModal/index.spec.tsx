9d7e3365b640cbf2da6b7ab335c10352947a9fa5
Igor Atilar <igoratilar10@gmail.com>
2022-08-18 09:12:50 -0300

    test: add react-player mock on VideoPlayerModal test

--- src/presentation/components/VideoPlayerModal/index.spec.tsx
+++ src/presentation/components/VideoPlayerModal/index.spec.tsx
@@ -1,8 +1,15 @@
 import userEvent from '@testing-library/user-event';
+import YouTubePlayer from 'react-player/youtube';
 import { act, render, screen } from '@/utils/test';
 import { VideoPlayerModal, VideoPlayerProps } from '.';
 
-jest.mock('react-player/youtube');
+jest.mock(
+  'react-player/youtube',
+  () =>
+    function () {
+      return <div>mock-youtube-player</div>;
+    }
+);
 
 describe('Presentation: Components/VideoPlayerModal', () => {
   const defaultProps: VideoPlayerProps = {
@@ -13,9 +20,9 @@ describe('Presentation: Components/VideoPlayerModal', () => {
 
   it('should render the initial state correctly', () => {
     render(<VideoPlayerModal {...defaultProps} />);
-    const pauseButton = screen.getByRole('button', { name: 'Pausar' });
-    const progressInput = screen.getByRole('slider', { name: 'Duração' });
-    const volumeInput = screen.getByRole('slider', { name: 'Volume' });
+    const pauseButton = screen.getByRole('button', { name: 'pausar' });
+    const progressInput = screen.getByRole('slider', { name: 'duração' });
+    const volumeInput = screen.getByRole('slider', { name: 'volume' });
     expect(progressInput).toHaveValue('0');
     expect(pauseButton).toBeInTheDocument();
     expect(volumeInput).toHaveValue('1');
@@ -24,22 +31,22 @@ describe('Presentation: Components/VideoPlayerModal', () => {
   it('should render the play button when the pause button is pressed', async () => {
     const user = userEvent.setup();
     render(<VideoPlayerModal {...defaultProps} />);
-    const pauseButton = screen.getByRole('button', { name: 'Pausar' });
+    const pauseButton = screen.getByRole('button', { name: 'pausar' });
     await act(async () => {
       await user.click(pauseButton);
     });
-    const playButton = screen.getByRole('button', { name: 'Reproduzir' });
+    const playButton = screen.getByRole('button', { name: 'reproduzir' });
     expect(playButton).toBeInTheDocument();
   });
 
   it('should render the pause button when the play button is pressed', async () => {
     const user = userEvent.setup();
     render(<VideoPlayerModal {...defaultProps} />);
-    const pauseButton = screen.getByRole('button', { name: 'Pausar' });
+    const pauseButton = screen.getByRole('button', { name: 'pausar' });
     await act(async () => {
       await user.click(pauseButton);
     });
-    const playButton = screen.getByRole('button', { name: 'Reproduzir' });
+    const playButton = screen.getByRole('button', { name: 'reproduzir' });
     await act(async () => {
       await user.click(playButton);
     });
