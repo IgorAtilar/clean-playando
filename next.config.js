731477874ae01ce4556eae34e40f61114181d5aa
Igor Atilar <igoratilar10@gmail.com>
2022-10-09 17:04:45 -0300

    chore: add svgr config on next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,29 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      loader: '@svgr/webpack',
+      options: {
+        prettier: false,
+        svgo: true,
+        svgoConfig: {
+          plugins: [
+            {
+              name: 'preset-default',
+              params: {
+                overrides: { removeViewBox: false }
+              }
+            }
+          ]
+        },
+        titleProp: true
+      },
+      test: /\.svg$/
+    });
+
+    return config;
+  },
   compiler: {
     styledComponents: true
   }
